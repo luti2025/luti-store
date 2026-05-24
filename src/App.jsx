e67e3885d22a345e9fddb7d0cca5d3902e3f23eb
Luti Admin <dendaljose@gmail.com>
2026-05-23 21:43:05 -0300

    Fix: Corregir error de sintaxis en div

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
       <p className="text-xl text-zinc-400 italic">Estamos preparando algo increíble. <br/> Volvemos muy pronto.</p>
       <div className="mt-8 animate-bounce">
         <span className="text-4xl">🧵</span>
-      </div_div>
+      </div>
     </div>
   );
 }
